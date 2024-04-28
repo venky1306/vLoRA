082a47c0759fc7348880adfca64b9270bc43e49a
venky1306 <47674519+venky1306@users.noreply.github.com>
2024-04-28 03:09:17 -0500

    version.txt

--- setup.py
+++ setup.py
@@ -97,7 +97,6 @@ def get_local_version_suffix() -> str:
 
 
 def get_version() -> str:
-    version = os.getenv("PUNICA_BUILD_VERSION")
     if version is None:
         with open(root / "version.txt") as f:
             version = f.read().strip()
